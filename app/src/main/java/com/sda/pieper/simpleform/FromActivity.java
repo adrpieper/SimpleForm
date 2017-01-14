2e2c08c1d501825c2e0dca5cd7656a0f5f514960
Adrian <adrpieper@gmail.com>
2017-01-14 10:44:04 +0100

    Dodałem główną aktywność

---
+++ app/src/main/java/com/sda/pieper/simpleform/FromActivity.java
@@ -0,0 +1,13 @@
+package com.sda.pieper.simpleform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FromActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_from);
+    }
+}
