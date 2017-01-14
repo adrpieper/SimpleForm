52cbdaef2f885964c4229f4633b5113683ede37e
Adrian <adrpieper@gmail.com>
2017-01-14 12:29:27 +0100

    Zmieniłem zmienne lokalne na pola klasy - (Crtl+Alt+f)

--- app/src/main/java/com/sda/pieper/simpleform/FromActivity.java
+++ app/src/main/java/com/sda/pieper/simpleform/FromActivity.java
@@ -2,6 +2,8 @@ package com.sda.pieper.simpleform;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -9,28 +11,57 @@ import android.widget.RadioGroup;
 
 public class FromActivity extends Activity {
 
+    private EditText userNameEditText;
+    private EditText userSurnameEditText;
+    private EditText userBirthDateEditText;
+    private RadioGroup sexRadioGroup;
+    private RadioButton sexFemaleRadioButton;
+    private RadioButton sexMaleRadioButton;
+    private EditText streetEditText;
+    private EditText houseNumberEditText;
+    private EditText postCodeEditText;
+    private EditText cityEditText;
+    private EditText stateEditText;
+    private EditText emailEditText;
+    private EditText phoneNumberEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private CheckBox newsletterCheckBox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_from);
 
-        EditText userNameEditText = (EditText) findViewById(R.id.user_name_edit_text);
-        EditText userSurnameEditText = (EditText) findViewById(R.id.user_surname_edit_text);
-        EditText userBithDateEditText = (EditText) findViewById(R.id.user_birth_date_edit_text);
-        RadioGroup sexRadioGroup = (RadioGroup) findViewById(R.id.sex_radio_group);
+        userNameEditText = (EditText) findViewById(R.id.user_name_edit_text);
+        userSurnameEditText = (EditText) findViewById(R.id.user_surname_edit_text);
+        userBirthDateEditText = (EditText) findViewById(R.id.user_birth_date_edit_text);
+        sexRadioGroup = (RadioGroup) findViewById(R.id.sex_radio_group);
 
-        RadioButton sexFemaleRadioButton = (RadioButton) findViewById(R.id.sex_female);
-        RadioButton sexMaleRadioButton = (RadioButton) findViewById(R.id.sex_male);
+        sexFemaleRadioButton = (RadioButton) findViewById(R.id.sex_female);
+        sexMaleRadioButton = (RadioButton) findViewById(R.id.sex_male);
 
-        EditText streetEditText = (EditText) findViewById(R.id.street_edit_text);
-        EditText houseNumberEditText = (EditText) findViewById(R.id.house_number_edit_text);
-        EditText postCodeEditText = (EditText) findViewById(R.id.post_code_edit_text);
-        EditText cityEditText = (EditText) findViewById(R.id.city_edit_text);
-        EditText state_edit_text = (EditText) findViewById(R.id.state_edit_text);
-        EditText email_edit_text = (EditText) findViewById(R.id.email_edit_text);
-        EditText phone_number_edit_text = (EditText) findViewById(R.id.phone_number_edit_text);
-        EditText password_edit_text = (EditText) findViewById(R.id.password_edit_text);
-        EditText confirm_password_edit_text = (EditText) findViewById(R.id.confirm_password_edit_text);
-        CheckBox newsletter_check_box = (CheckBox) findViewById(R.id.newsletter_check_box);
+        streetEditText = (EditText) findViewById(R.id.street_edit_text);
+        houseNumberEditText = (EditText) findViewById(R.id.house_number_edit_text);
+        postCodeEditText = (EditText) findViewById(R.id.post_code_edit_text);
+        cityEditText = (EditText) findViewById(R.id.city_edit_text);
+        stateEditText =  (EditText) findViewById(R.id.state_edit_text);
+        emailEditText = (EditText) findViewById(R.id.email_edit_text);
+        phoneNumberEditText = (EditText) findViewById(R.id.phone_number_edit_text);
+        passwordEditText = (EditText) findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = (EditText) findViewById(R.id.confirm_password_edit_text);
+        newsletterCheckBox = (CheckBox) findViewById(R.id.newsletter_check_box);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateData();
+            }
+        });
+    
     }
-}
+
+    private void validateData() {
+    }
+}   
