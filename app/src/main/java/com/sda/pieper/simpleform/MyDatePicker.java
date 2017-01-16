12d8cbea98ad4c697d25ecf96a83980da2353667
Adrian <adrpieper@gmail.com>
2017-01-16 20:34:45 +0100

    Dadałem wyświetlanie loga po wciśnieciu MyDatePicker

--- app/src/main/java/com/sda/pieper/simpleform/MyDatePicker.java
+++ app/src/main/java/com/sda/pieper/simpleform/MyDatePicker.java
@@ -2,6 +2,8 @@ package com.sda.pieper.simpleform;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -36,5 +38,15 @@ public class MyDatePicker extends TextView {
         calendar = Calendar.getInstance();
         format = new SimpleDateFormat("dd-MM-yyyy");
         setText(format.format(calendar.getTime()));
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    private void showDatePickerDialog(){
+        Log.d("My Date Picker", "showDatePickerDialog");
     }
 }
